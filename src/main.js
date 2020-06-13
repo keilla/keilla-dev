43abd6be1e496d3754414d401a30b8863ab772cb
Keilla Menezes Fernandes <keilla@gmail.com>
2020-06-13 15:49:08 -0300

    Adds button top

--- src/main.js
+++ src/main.js
@@ -6,6 +6,20 @@ import bio from './app/bio.html';
 import activities from './app/activities.html';
 import footer from './app/footer.html';
 
+window.onload = function() {
+  addNavBar();
+  addSections();
+  addClickTopButton();
+}
+
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function addNavBar() {
+  document.getElementById("root").innerHTML = navBar;
+}
+
 function appendSection(component, postion='beforeend') {
   var node = document.createElement("div");
   node.className = "section";
@@ -13,10 +27,28 @@ function appendSection(component, postion='beforeend') {
   document.getElementById("root").insertAdjacentElement(postion, node);
 }
 
-window.onload = function() {
-  document.getElementById("root").innerHTML = navBar;
+function addSections() {
   appendSection(hero);
   appendSection(bio);
   appendSection(activities);
   appendSection(footer, 'afterEnd');
+}
+
+function addClickTopButton() {
+  const topButton = document.getElementById("top-button");
+  topButton.onclick = topFunction;
+}
+
+function scrollFunction() {
+  const topButton = document.getElementById("top-button");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
