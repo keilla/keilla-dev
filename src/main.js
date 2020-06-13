771152da015b53ac195025d46e5d456ad1e873b9
Keilla Menezes Fernandes <keilla@gmail.com>
2020-06-13 14:45:05 -0300

    Adds footer

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import './styles/style.scss';
 import navBar from './app/nav-bar.html';
 import hero from './app/hero.html';
 import bio from './app/bio.html';
-import activities from './app/activities.html'
+import activities from './app/activities.html';
+import footer from './app/footer.html';
 
-function appendSection(component) {
+function appendSection(component, postion='beforeend') {
   var node = document.createElement("div");
   node.className = "section";
   node.innerHTML = component;
-  document.getElementById("root").insertAdjacentElement('beforeend', node);
+  document.getElementById("root").insertAdjacentElement(postion, node);
 }
 
 window.onload = function() {
@@ -17,4 +18,5 @@ window.onload = function() {
   appendSection(hero);
   appendSection(bio);
   appendSection(activities);
+  appendSection(footer, 'afterEnd');
 }
